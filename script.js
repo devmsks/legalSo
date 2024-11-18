6296d1d6761a0f876ec81968cbf47978972e4538
devmsks <khushisharma93267@gmail.com>
2024-11-18 02:22:17 -0800

    all files added

---
+++ script.js
@@ -0,0 +1,29 @@
+$('.legal_grid').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows:true,
+    prevArrow:`<button type="button" class="slick-prev my-arrow">Previous</button>`,
+    nextArrow:`<button type="button" class="slick-next my-arrow">Next</button>`
+  });
+
+
+
+  $('.testimonial-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows:true,
+  });
+
+$('.client_grid').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows:true,
+    prevArrow:`<button type="button" class="slick-prev my-arrow">Previous</button>`,
+    nextArrow:`<button type="button" class="slick-next my-arrow">Next</button>`
+  });
